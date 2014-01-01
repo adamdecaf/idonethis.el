53e1a8fc7e9d71d6a2823f4f1b8aa1028fa78a4d
Adam Shannon <adam@ashannon.us>
2013-12-31 20:50:52 -0600

    adding a sorta hacky way to get all dones for a list.

--- idonethis.el
+++ idonethis.el
@@ -6,8 +6,30 @@
   "idonethis"
   :group 'applications)
 
+(defconst cookie-header "Cookie: sessionid=hbq8qeymypwy6amhiierfhcianbdbq6t;")
+(defconst base-idonethis-url "https://idonethis.com/api/v3/team/banno-grip/dones/?start=%s&end=%s")
+(defconst base-command-string "curl '%s' -H '%s' --compressed | json -a owner done_date markedup_text | sort | sed 's/$/\\n\\n/g'")
+
 (defun idonethis-send (stuff)
   (interactive "sWhat have you done today?: ")
   (shell-command (format "idonethis \"%s\"&" (shell-quote-argument stuff))))
+
+(defun idonethis-dones (&optional maybe-start-date &optional maybe-end-date)
+  (interactive)
+  (let* ((start-date (if maybe-start-date
+                         maybe-start-date
+                       (format-time-string "%Y-%m-%d")))
+        (end-date (if maybe-end-date
+                      maybe-end-date
+                    (format-time-string "%Y-%m-%d")))
+        (url (format base-idonethis-url start-date end-date))
+        (command (format base-command-string url cookie-header))
+        (command-res (shell-command-to-string (concat command "&")))
+        (idonethis-buffer (find-file (concat "idonethis-done-" start-date "-to-" end-date))))
+    (switch-to-buffer idonethis-buffer nil t)
+    (erase-buffer)
+    (insert command-res)
+    (delete-other-windows)
+    (goto-line 4)))
 
 (provide 'idonethis)
