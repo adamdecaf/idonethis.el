2766f2bb7bb098dc85978bded43f1b924280177a
Adam Shannon <adam@ashannon.us>
2013-12-31 19:09:31 -0600

    more stuff.

--- idonethis.el
+++ idonethis.el
@@ -2,6 +2,10 @@
 
 ;; Some methods that wrap the cli tool
 
+(defgroup idonethis nil
+  "idonethis"
+  :group 'applications)
+
 (defun idonethis-send (stuff)
   (interactive "sWhat have you done today?: ")
   (shell-command (format "idonethis '%s'" stuff)))
